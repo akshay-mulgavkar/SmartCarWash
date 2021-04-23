d54386008c2942ba8a4bfd6b8c1527ce569fc154
ReynoldVaz <54735127+ReynoldVaz@users.noreply.github.com>
2021-04-23 22:21:22 +0530

    Update ProfileScreen.js

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,62 +1,51 @@
-import {NavigationContainer} from "@react-navigation/native";
-import {createDrawerNavigator} from "@react-navigation/drawer";
-import AreaScreen from "./AreaScreen";
-import CarTypeScreen from "./CarTypeScreen";
-import HomeScreen from "./HomeScreen";
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import TextInput from '../components/TextInput'
 
-import React, {useState} from "react";
-import {View, StyleSheet, TouchableOpacity} from "react-native";
-import {Text} from "react-native-paper";
-import Background from "../components/Background";
-import Logo from "../components/Logo";
-import Header from "../components/Header";
-import Button from "../components/Button";
-import TextInput from "../components/TextInput";
-import BackButton from "../components/BackButton";
-import {theme} from "../core/theme";
-import {emailValidator} from "../helpers/emailValidator";
-import {passwordValidator} from "../helpers/passwordValidator";
-import {nameValidator} from "../helpers/nameValidator";
-import {Snackbar, Headline} from "react-native-paper";
-//import AccountCircleIcon from '@material-ui/icons/AccountCircle'
-import {Avatar} from "react-native-paper";
+import { theme } from '../core/theme'
+import { emailValidator } from '../helpers/emailValidator'
+import { passwordValidator } from '../helpers/passwordValidator'
+import { nameValidator } from '../helpers/nameValidator'
+import { Snackbar, Headline } from 'react-native-paper'
+import { Avatar } from 'react-native-paper'
 
-const Profile = ({navigation}) => {
-  const [name, setName] = useState({value: "", error: ""});
-  const [email, setEmail] = useState({value: "", error: ""});
-  const [password, setPassword] = useState({value: "", error: ""});
+const Profile = ({ navigation }) => {
+  const [name, setName] = useState({ value: '', error: '' })
+  const [email, setEmail] = useState({ value: '', error: '' })
+  const [password, setPassword] = useState({ value: '', error: '' })
 
   const onSignUpPressed = () => {
-    const nameError = nameValidator(name.value);
-    const emailError = emailValidator(email.value);
-    const passwordError = passwordValidator(password.value);
+    const nameError = nameValidator(name.value)
+    const emailError = emailValidator(email.value)
+    const passwordError = passwordValidator(password.value)
 
     if (emailError || passwordError || nameError) {
-      setName({...name, error: nameError});
-      setEmail({...email, error: emailError});
-      setPassword({...password, error: passwordError});
+      setName({ ...name, error: nameError })
+      setEmail({ ...email, error: emailError })
+      setPassword({ ...password, error: passwordError })
 
-      return;
+      return
     } else {
-      onToggleSnackBar();
+      onToggleSnackBar()
     }
-  };
-  const [visible, setVisible] = React.useState(false);
+  }
+  const [visible, setVisible] = React.useState(false)
 
-  const onToggleSnackBar = () => setVisible(!visible);
+  const onToggleSnackBar = () => setVisible(!visible)
 
-  const onDismissSnackBar = () => setVisible(false);
+  const onDismissSnackBar = () => setVisible(false)
 
   return (
     <View style={styles.container1}>
-      <BackButton goBack={navigation.goBack} />
       <Avatar.Text style={styles.container2} size={100} label="A" />
       <Header style={styles.container3}>Edit Profile</Header>
       <TextInput
         label="Name"
         returnKeyType="next"
         value={name.value}
-        onChangeText={(text) => setName({value: text, error: ""})}
+        onChangeText={(text) => setName({ value: text, error: '' })}
         error={!!name.error}
         errorText={name.error}
       />
@@ -64,7 +53,7 @@ const Profile = ({navigation}) => {
         label="Email"
         returnKeyType="next"
         value={email.value}
-        onChangeText={(text) => setEmail({value: text, error: ""})}
+        onChangeText={(text) => setEmail({ value: text, error: '' })}
         error={!!email.error}
         errorText={email.error}
         autoCapitalize="none"
@@ -76,7 +65,7 @@ const Profile = ({navigation}) => {
         label="Password"
         returnKeyType="done"
         value={password.value}
-        onChangeText={(text) => setPassword({value: text, error: ""})}
+        onChangeText={(text) => setPassword({ value: text, error: '' })}
         error={!!password.error}
         errorText={password.error}
         secureTextEntry
@@ -85,7 +74,7 @@ const Profile = ({navigation}) => {
         label="Confirm Password"
         returnKeyType="done"
         value={password.value}
-        onChangeText={(text) => setPassword({value: text, error: ""})}
+        onChangeText={(text) => setPassword({ value: text, error: '' })}
         error={!!password.error}
         errorText={password.error}
         secureTextEntry
@@ -94,23 +83,25 @@ const Profile = ({navigation}) => {
         mode="contained"
         //onPress={onToggleSnackBar}
         onPress={onSignUpPressed}
-        style={{marginTop: 24}}>
+        style={{ marginTop: 24 }}
+      >
         Save Changes
       </Button>
       <Snackbar
         visible={visible}
         onDismiss={onDismissSnackBar}
         action={{
-          label: "OK",
+          label: 'OK',
           onPress: () => {
             // Do something
           },
-        }}>
+        }}
+      >
         Changes Saved.
       </Snackbar>
     </View>
-  );
-};
+  )
+}
 const styles = StyleSheet.create({
   container1: {
     flex: 1,
@@ -118,31 +109,26 @@ const styles = StyleSheet.create({
     //marginTop: 4,
   },
   container2: {
-    //flex: 1,
-    //padding: 20,
-    //marginTop: 4,
-    //paddingHorizontal: 20,
-    //paddingVertical: 5,
-    marginVertical: 40,
+    marginVertical: 10,
     marginHorizontal: 130,
 
     fontSize: 70,
   },
   row: {
-    flexDirection: "row",
+    flexDirection: 'row',
     marginTop: 4,
   },
   link: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     color: theme.colors.primary,
   },
   container3: {
-    textAlignVertical: "center",
-    textAlign: "center",
+    textAlignVertical: 'center',
+    textAlign: 'center',
     fontSize: 20,
-    fontWeight: "bold",
-    color: "#560CCE",
+    fontWeight: 'bold',
+    color: '#560CCE',
   },
-});
+})
 
-export default Profile;
+export default Profile
