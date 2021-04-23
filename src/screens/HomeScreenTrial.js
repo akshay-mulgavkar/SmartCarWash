244ccb07afd5f9b896ee11e0ad7f461b1900d3d4
ReynoldVaz <54735127+ReynoldVaz@users.noreply.github.com>
2021-04-23 22:19:23 +0530

    Update HomeScreenTrial.js

--- src/screens/HomeScreenTrial.js
+++ src/screens/HomeScreenTrial.js
@@ -1,27 +1,9 @@
-import {NavigationContainer} from "@react-navigation/native";
-import {createDrawerNavigator} from "@react-navigation/drawer";
-import AreaScreen from "./AreaScreen";
-import CarTypeScreen from "./CarTypeScreen";
-import HomeScreen from "./HomeScreen";
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import HomeScreen from './HomeScreen'
+import React from 'react'
 
-import React, {useState} from "react";
-import {View, StyleSheet, TouchableOpacity} from "react-native";
-import {Text} from "react-native-paper";
-import Background from "../components/Background";
-import Logo from "../components/Logo";
-import Header from "../components/Header";
-import Button from "../components/Button";
-import TextInput from "../components/TextInput";
-import BackButton from "../components/BackButton";
-import {theme} from "../core/theme";
-import {emailValidator} from "../helpers/emailValidator";
-import {passwordValidator} from "../helpers/passwordValidator";
-import {nameValidator} from "../helpers/nameValidator";
-import {Snackbar, Headline} from "react-native-paper";
-//import AccountCircleIcon from '@material-ui/icons/AccountCircle'
-import {Avatar} from "react-native-paper";
-
-const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator()
 
 function MyDrawer() {
   return (
@@ -32,7 +14,7 @@ function MyDrawer() {
       <Drawer.Screen name="Help" component={Help} />
       <Drawer.Screen name="Sign Out" component={SignOut} />
     </Drawer.Navigator>
-  );
+  )
 }
 
 export default function HomeScreenTrial() {
@@ -40,5 +22,5 @@ export default function HomeScreenTrial() {
     <NavigationContainer independent={true}>
       <MyDrawer />
     </NavigationContainer>
-  );
+  )
 }
