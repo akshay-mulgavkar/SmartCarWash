940f3f7d192054254b62465297154fa075af018e
ReynoldVaz <54735127+ReynoldVaz@users.noreply.github.com>
2021-04-23 22:17:35 +0530

    Update HelpScreen.js

--- src/screens/HelpScreen.js
+++ src/screens/HelpScreen.js
@@ -1,33 +1,40 @@
-import {NavigationContainer} from "@react-navigation/native";
-import {createDrawerNavigator} from "@react-navigation/drawer";
-import AreaScreen from "./AreaScreen";
-import CarTypeScreen from "./CarTypeScreen";
-import HomeScreen from "./HomeScreen";
+import React, { useState } from 'react'
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { theme } from '../core/theme'
 
-import React, {useState} from "react";
-import {View, StyleSheet, TouchableOpacity} from "react-native";
-import {Text} from "react-native-paper";
-import Background from "../components/Background";
-import Logo from "../components/Logo";
-import Header from "../components/Header";
-import Button from "../components/Button";
-import TextInput from "../components/TextInput";
-import BackButton from "../components/BackButton";
-import {theme} from "../core/theme";
-import {emailValidator} from "../helpers/emailValidator";
-import {passwordValidator} from "../helpers/passwordValidator";
-import {nameValidator} from "../helpers/nameValidator";
-import {Snackbar, Headline} from "react-native-paper";
-//import AccountCircleIcon from '@material-ui/icons/AccountCircle'
-import {Avatar} from "react-native-paper";
-
-const Help = ({navigation}) => {
+function Help() {
   return (
     <View>
-      <Headline style={styles.container3}>Help</Headline>
+      <Text
+        style={{
+          marginLeft: 130,
+          marginTop: 150,
+          fontWeight: 'bold',
+          color: 'black',
+          fontSize: 20,
+        }}
+      >
+        Visit Our Website :{'\n'}
+        {'\n'} Link
+      </Text>
+      <Text style={{ marginLeft: 100, marginTop: 50, color: 'black' }}>
+        -------------------OR----------------------
+      </Text>
+      <Text
+        style={{
+          marginLeft: 135,
+          marginTop: 50,
+          fontWeight: 'bold',
+          color: 'black',
+          fontSize: 20,
+        }}
+      >
+        Contact Us At :{'\n'}
+        {'\n'} 9657712712
+      </Text>
     </View>
-  );
-};
+  )
+}
 const styles = StyleSheet.create({
   container1: {
     flex: 1,
@@ -35,31 +42,26 @@ const styles = StyleSheet.create({
     //marginTop: 4,
   },
   container2: {
-    //flex: 1,
-    //padding: 20,
-    //marginTop: 4,
-    //paddingHorizontal: 20,
-    //paddingVertical: 5,
     marginVertical: 40,
     marginHorizontal: 130,
 
     fontSize: 70,
   },
   row: {
-    flexDirection: "row",
+    flexDirection: 'row',
     marginTop: 4,
   },
   link: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     color: theme.colors.primary,
   },
   container3: {
-    textAlignVertical: "center",
-    textAlign: "center",
+    textAlignVertical: 'center',
+    textAlign: 'center',
     fontSize: 20,
-    fontWeight: "bold",
-    color: "#560CCE",
+    fontWeight: 'bold',
+    color: '#560CCE',
   },
-});
+})
 
-export default Help;
+export default Help
