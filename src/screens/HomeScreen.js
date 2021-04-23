bf0c7d85f17eea4c2bdb87ce0d8fb222a1a50f17
ReynoldVaz <54735127+ReynoldVaz@users.noreply.github.com>
2021-04-24 01:15:54 +0530

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,9 @@
 //import React from 'react'
-import Background from "../components/Background";
-import Logo from "../components/Logo";
-import Header from "../components/Header";
-import Paragraph from "../components/Paragraph";
-import Button from "../components/Button";
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Paragraph from '../components/Paragraph'
+import Button from '../components/Button'
 import {
   FlatList,
   StyleSheet,
@@ -15,35 +15,35 @@ import {
   SafeAreaView,
   StatusBar,
   ScrollView,
-} from "react-native";
-import BackButton from "../components/BackButton";
-import React, {useState} from "react";
-import {getStatusBarHeight} from "react-native-status-bar-height";
-import {Divider, Headline, Drawer} from "react-native-paper";
+} from 'react-native'
+import BackButton from '../components/BackButton'
+import React, { useState } from 'react'
+import { getStatusBarHeight } from 'react-native-status-bar-height'
+import { Divider, Headline, Drawer } from 'react-native-paper'
 
-import {createDrawerNavigator} from "@react-navigation/drawer";
-import {NavigationContainer} from "@react-navigation/native";
-import AreaScreen from "./AreaScreen";
-import CarTypeScreen from "./CarTypeScreen";
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { NavigationContainer } from '@react-navigation/native'
+import AreaScreen from './AreaScreen'
+import CarTypeScreen from './CarTypeScreen'
 
-const HomeScreen1 = ({navigation}) => {
+const HomeScreen1 = ({ navigation }) => {
   return (
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button
-        onPress={() => navigation.navigate("Notifications")}
+        onPress={() => navigation.navigate('Notifications')}
         title="Go to notifications"
       />
     </View>
-  );
-};
+  )
+}
 
-const NotificationsScreen = ({navigation}) => {
+const NotificationsScreen = ({ navigation }) => {
   return (
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button onPress={() => navigation.goBack()} title="Go back home" />
     </View>
-  );
-};
+  )
+}
 
 {
   /*const AreaScreen = ({ navigation }) => (
@@ -78,62 +78,62 @@ const NotificationsScreen = ({navigation}) => {
 }
 const DATA = [
   {
-    id: "1",
-    title: "Sparkle And Shine ",
+    id: '1',
+    title: 'Sparkle And Shine ',
   },
   {
-    id: "2",
-    title: "Sai Car Washing Centre",
+    id: '2',
+    title: 'Sai Car Washing Centre',
   },
   {
-    id: "3",
+    id: '3',
     title: "Sunny's Washing Centre",
   },
   {
-    id: "4",
+    id: '4',
     title: "Sunny's Washing Centre",
   },
   {
-    id: "5",
+    id: '5',
     title: "Sunny's Washing Centre",
   },
   {
-    id: "6",
+    id: '6',
     title: "Sunny's Washing Centre",
   },
   {
-    id: "7",
+    id: '7',
     title: "Sunny's Washing Centre",
   },
   {
-    id: "8",
+    id: '8',
     title: "Sunny's Washing Centre",
   },
-];
+]
 
-const Item = ({item, onPress, style}) => (
+const Item = ({ item, onPress, style }) => (
   <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
     <Text style={styles.title}>{item.title}</Text>
   </TouchableOpacity>
-);
+)
 
-const HomeScreen = ({route, navigation}) => {
-  const [active, setActive] = React.useState("");
-  const [selectedId, setSelectedId] = useState(null);
+const HomeScreen = ({ route, navigation }) => {
+  const [active, setActive] = React.useState('')
+  const [selectedId, setSelectedId] = useState(null)
 
-  const [area, setSelectedArea] = useState(null);
-  const [carType, setSelectedCarType] = useState(null);
+  const [area, setSelectedArea] = useState(null)
+  const [carType, setSelectedCarType] = useState(null)
 
   try {
-    if (typeof route === "object" && route !== null) {
-      if ("selectedArea" in route.params) {
+    if (typeof route === 'object' && route !== null) {
+      if ('selectedArea' in route.params) {
         // alert(route.params.selectedArea.title);
         if (route.params.selectedArea != null) {
           if (area == null) {
-            setSelectedArea(route.params.selectedArea);
+            setSelectedArea(route.params.selectedArea)
           } else {
             if (area.id != route.params.selectedArea.id) {
-              setSelectedArea(route.params.selectedArea);
+              setSelectedArea(route.params.selectedArea)
             }
           }
         }
@@ -141,115 +141,112 @@ const HomeScreen = ({route, navigation}) => {
     }
 
     if (route.params.selectedCarType != null) {
-      if ("selectedCarType" in route.params) {
+      if ('selectedCarType' in route.params) {
         // alert(route.params.selectedCarType.title);
 
         if (carType == null) {
-          setSelectedCarType(route.params.selectedCarType);
+          setSelectedCarType(route.params.selectedCarType)
         } else {
           if (carType.id != route.params.selectedCarType.id) {
-            setSelectedCarType(route.params.selectedCarType);
+            setSelectedCarType(route.params.selectedCarType)
           }
         }
       }
     }
   } catch (err) {
-    console.error(err);
+    console.error(err)
     // alert(err);
   }
 
-  const renderItem = ({item}) => {
-    const backgroundColor = item.id === selectedId ? "lightgray" : "white";
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? 'lightgray' : 'white'
 
     return (
       <Item
         item={item}
-        onPress={() => navigation.navigate("CarCentreScreen")}
-        style={{backgroundColor}}
+        //  onPress={() => navigation.navigate('CarCentreScreen')}
+
+        onPress={() =>
+          navigation.navigate('CarCentreScreen', {
+            params: { selectedCarCentre: item },
+          })
+        }
+        style={{ backgroundColor }}
       />
-    );
-  };
+    )
+  }
 
   return (
     <ScrollView style={styles.container1}>
-      {/* <NavigationContainer independent={true}>
-        <MyDrawer />
-      </NavigationContainer> */}
-      <Headline
-        style={{
-          textAlignVertical: "center",
-          textAlign: "center",
-          fontSize: 30,
-          paddingTop: 30,
-        }}>
-        Smart Car Wash
-      </Headline>
-
       {area == null ? (
         <Button
-          style={[{width: "50%", marginHorizontal: 90}]}
+          style={[{ width: '50%', marginHorizontal: 90 }]}
           mode="contained"
-          onPress={() => navigation.navigate("AreaScreen")}>
+          onPress={() => navigation.navigate('AreaScreen')}
+        >
           Area
         </Button>
       ) : (
         <View
           style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-evenly",
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
 
-            marginVertical: 22,
-          }}>
-          <Text style={{fontSize: 22}}>{area.title}</Text>
-          <TouchableOpacity
+            // marginVertical: 10,
+          }}
+        >
+          <Button
+            style={[{ width: '50%', marginHorizontal: 90 }]}
+            mode="contained"
             onPress={() => {
-              route.params.selectedArea = null;
-              setSelectedArea(null);
-            }}>
-            <Text style={{fontSize: 22}}> X</Text>
-          </TouchableOpacity>
+              route.params.selectedArea = null
+              setSelectedArea(null)
+            }}
+          >
+            {area.title} {'       '} x
+          </Button>
         </View>
       )}
 
       {carType == null ? (
         <Button
-          style={[{width: "50%", marginHorizontal: 90}]}
+          style={[{ width: '50%', marginHorizontal: 90 }]}
           //style={[{ align: 'centre' }]}
           mode="contained"
-          onPress={() => navigation.navigate("CarTypeScreen")}>
+          onPress={() => navigation.navigate('CarTypeScreen')}
+        >
           Car Type
         </Button>
       ) : (
         <View
           style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            marginVertical: 22,
-          }}>
-          <Text style={{fontSize: 22}}>{carType.title}</Text>
-          <TouchableOpacity
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            // marginVertical: 10,
+          }}
+        >
+          <Button
+            style={[{ width: '50%', marginHorizontal: 90 }]}
+            mode="contained"
             onPress={() => {
-              route.params.selectedCarType = null;
-              setSelectedCarType(null);
-            }}>
-            <Text style={{fontSize: 22}}> X</Text>
-          </TouchableOpacity>
+              route.params.selectedCarType = null
+              setSelectedCarType(null)
+            }}
+          >
+            {carType.title}
+            {'        '} x
+          </Button>
         </View>
       )}
-      <Button
-        style={[{width: "50%", marginHorizontal: 90}]}
-        mode="contained"
-        onPress={() => navigation.openDrawer()}>
-        Drawer
-      </Button>
 
       <FlatList
         style={{
           // textAlignVertical: 'center',
           // textAlign: 'center',
           fontSize: 30,
+          marginRight: 15,
           // paddingTop: 30,
         }}
         data={DATA}
@@ -258,8 +255,8 @@ const HomeScreen = ({route, navigation}) => {
         extraData={selectedId}
       />
     </ScrollView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container1: {
@@ -269,24 +266,14 @@ const styles = StyleSheet.create({
     //paddingTop: 45,
     paddingTop: 30,
     paddingLeft: 20,
-    alignContent: "center",
+    alignContent: 'center',
   },
   container2: {
-    //flex: 1,
-    //marginTop: StatusBar.currentHeight || 0,
-
-    //paddingTop: 30,
-    //paddingLeft: 20,
-    alignItems: "center",
-    //flexDirection: 'row',
+    alignItems: 'center',
     paddingTop: 20,
     padding: 10,
   },
   container3: {
-    //flex: 1,
-    //marginTop: StatusBar.currentHeight || 0,
-
-    //paddingTop: 45,
     paddingTop: 5,
 
     //alignItems: 'center',
@@ -305,6 +292,6 @@ const styles = StyleSheet.create({
   button: {
     paddingHorizontal: 30,
   },
-});
+})
 
-export default HomeScreen;
+export default HomeScreen
